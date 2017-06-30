32e0bcbab5d5949afa08ddc63d9fdbe6ca945c46
nasuton <lovetennis_airu@yahoo.co.jp>
2017-06-30 17:36:21 +0900

    libAdd

---
+++ cocos2d/cocos/platform/android/java/.d
@@ -0,0 +1,4 @@
+C:\cocos2d-x_Project\CoinGame\cocos2d\cocos\platform\android\java\gen\com\enhance\gameservice\IGameTuningService.java : \
+  C:\cocos2d-x_Project\CoinGame\cocos2d\cocos\platform\android\java\src\com\enhance\gameservice\IGameTuningService.aidl
+
+C:\cocos2d-x_Project\CoinGame\cocos2d\cocos\platform\android\java\src\com\enhance\gameservice\IGameTuningService.aidl :
